554b85aa986c61d02ebc285b946ddc1d3ffd224a
Plastikpizza <lukas.hassel@student.uni-siegen.de>
2019-11-14 14:02:31 +0100

    day13

--- day13/day13.hs
+++ day13/day13.hs
@@ -29,70 +29,78 @@ fromTuple f (a,b) = f a b
 
 crash karts = or [fromTuple (==) a | a <- (pairs $ map pos karts)]
 
-drive k = k{pos=newPos} 
+drive kart = kart{pos=newPos} 
     where 
-        (x, y) = pos k
-        d      = dir k
+        (x, y) = pos kart
+        d      = dir kart
         newPos = case d of 
             LEFT -> (x-1, y)
             RIGHT -> (x+1, y)
             UP    -> (x  , y-1)
             DOWN  -> (x  , y+1)
 
-intersection k = k{dir=newDir, nxt=turnedNxt}
+intersection kart = kart{dir=newDir, nxt=turnedNxt}
     where
-        turnedNxt = next (nxt k)
-        newDir    = case (nxt k) of
-            STRAIGHT      -> (dir k)
-            LEFT_TURN     -> prev (dir k)
-            RIGHT_TURN    -> next (dir k)
+        turnedNxt = next (nxt kart)
+        newDir    = case (nxt kart) of
+            STRAIGHT      -> (dir kart)
+            LEFT_TURN     -> prev (dir kart)
+            RIGHT_TURN    -> next (dir kart)
 
-turn k@(Kart _ d _) '/' = case d of
-    LEFT  -> k{dir=DOWN}
-    UP    -> k{dir=RIGHT}
-    RIGHT -> k{dir=UP}
-    DOWN  -> k{dir=LEFT}
-turn k@(Kart _ d _) '\\' = case d of
-    LEFT  -> k{dir=UP}
-    UP    -> k{dir=LEFT}
-    RIGHT -> k{dir=DOWN}
-    DOWN  -> k{dir=RIGHT}
+turn kart@(Kart _ d _) '/' = case d of
+    LEFT  -> kart{dir=DOWN}
+    UP    -> kart{dir=RIGHT}
+    RIGHT -> kart{dir=UP}
+    DOWN  -> kart{dir=LEFT}
 
-move i k = drive step1
+turn kart@(Kart _ d _) '\\' = case d of
+    LEFT  -> kart{dir=UP}
+    UP    -> kart{dir=LEFT}
+    RIGHT -> kart{dir=DOWN}
+    DOWN  -> kart{dir=RIGHT}
+
+move i kart = drive step1
     where 
-        step1 = case (Map.lookup (pos k) i) of
-            Nothing  -> k
-            Just '+' -> intersection k
-            Just  a  -> turn k a
+        step1 = case (Map.lookup (pos kart) i) of
+            Nothing  -> kart
+            Just '+' -> intersection kart
+            Just  a  -> turn kart a
 
 sortGT (a1, b1) (a2, b2)
-  | b1 < b2 = GT
-  | b1 > b2 = LT
+  | b1 > b2 = GT
+  | b1 < b2 = LT
   | b1 == b2 = compare a1 a2
 
-moveAllOrCrash ms is [] = ms
-moveAllOrCrash ms is (k:ks) = case ms of 
-    Right a -> if crash (movedKart:a) || crash (movedKart:ks) then Left (movedKart, a, ks) else moveAllOrCrash (Right (movedKart:a)) is ks
-    otherwise -> ms
+moveAllOrCrash movedKarts tracks [] = movedKarts
+moveAllOrCrash movedKarts tracks (kart:karts) = case movedKarts of 
+    Right a ->  if crash (movedKart:a) || crash (movedKart:karts) 
+                then Left (movedKart, a, karts) 
+                else moveAllOrCrash (Right (movedKart:a)) tracks karts
+    otherwise -> movedKarts
     where 
-        movedKart = move is k
+        movedKart = move tracks kart
 
 pairs [] = []
 pairs (l:ls) = ((map (\b -> (l,b)) ls) ++ (pairs ls))  
 
-part1 ks is = case moveAllOrCrash (Right []) is (sortBy (\a b -> sortGT (pos a) (pos b)) ks) of 
+part1 karts tracks = case moveAllOrCrash (Right []) tracks (sortBy (\a b -> sortGT (pos a) (pos b)) karts) of 
         Left (p,_,_) -> print (pos p)
-        Right nks -> part1 nks is
+        Right nks -> part1 nks tracks
     
-part2 [k] is = print k
-part2 ks is = case moveAllOrCrash (Right []) is ks of
-    Right nks -> part2 nks is -- fine
+part2 [] tracks = putStrLn "all karts crashed."
+part2 [kart] tracks = print kart
+part2 karts tracks = 
+    case moveAllOrCrash (Right []) tracks (sortBy (\a b -> sortGT (pos a) (pos b)) karts) of
+    Right nks -> part2 nks tracks -- fine
     Left  (crashedKart, alreadyMoved, toBeMoved) -> do
-        part2 (helper [c | c<-alreadyMoved, (pos c) /= (pos crashedKart)] [c | c<-toBeMoved, (pos c) /= (pos crashedKart)]) is-- crash
+        putStrLn ("crash at " ++ (show $ pos $ crashedKart))
+        part2 
+            (helper [c | c<-alreadyMoved, (pos c) /= (pos crashedKart)] [c | c<-toBeMoved, (pos c) 
+            /= (pos crashedKart)]) tracks-- crash
     where
         helper :: [Kart] -> [Kart] -> [Kart]
         helper a [] = a
-        helper a b = case moveAllOrCrash (Right a) is b of
+        helper a b = case moveAllOrCrash (Right a) tracks b of
             Left (i,f,g) -> helper [c | c<-f, (pos c) /= (pos i)] [c | c<-g, (pos c) /= (pos i)]
             Right i -> i
 main = do
